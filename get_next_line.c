65f94166e864f8785a09d5d6131e01ee9228a53b
Claudio Matheus <claudio1.code@gmail.com>
2025-08-11 16:29:34 -0300

    amanha eu acabo

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,11 @@
 /*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 10:21:36 by clados-s          #+#    #+#             */
-/*   Updated: 2025/08/08 16:00:39 by clados-s         ###   ########.fr       */
+/*   Updated: 2025/08/11 16:27:48 by clados-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
 
 #include "get_next_line.h"
 
@@ -16,85 +18,89 @@ char	*get_next_line(int fd)
 {
 	static char	*rest;
 	char		*buffer;
+	int			bytes_buffer;
 	char		*line;
-	ssize_t		byte_buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	while (ft_strchar(rest, '\n') && byte_buffer > 0)
+	bytes_buffer = 1;
+	while (!ft_strchar(rest, '\n') && bytes_buffer > 0)
 	{
-		byte_buffer = read(fd, buffer, BUFFER_SIZE);
-		if (byte_buffer == -1)
+		bytes_buffer = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_buffer == -1)
 		{
 			free(buffer);
+			free(rest);
 			return (NULL);
 		}
-		buffer[byte_buffer] = '\0';
+		if (bytes_buffer == 0)
+			break ;
+		buffer[bytes_buffer] = '\0';
+		// printf("\ndebug\n\n");
 		rest = ft_strjoin_free(rest, buffer);
 	}
-	free(buffer);
+	free (buffer);
+	if (rest == NULL || rest[0] == '\0')
+		return (NULL);
 	line = get_line(rest);
-	rest = updated_rest(rest);
+	rest = update_rest(rest);
 	return (line);
 }
 
 char	*get_line(char *rest)
 {
-	char	*line;
 	int		i;
-
-	i = 0;
-	if (!rest[i])
-		return (NULL);
-	while (rest[i] && rest[i] != '\n')
-		i++;
-	line = (char *)malloc(sizeof(char) * (i + 1));
-	if (!line)
-		return (NULL);
-	i = -1;
-	while (rest[++i] && rest[i] != '\n')
-		line[i] = rest[i];
-	if (rest[i] == '\n')
-		line[i++] = '\n';
-	line[i++] = '\0';
-	return (line);
-}
-
-char	*updated_rest(char *rest)
-{
-	char	*new_rest;
-	int		i;
-	int		j;
-
+	char	*new_line;
+	
 	if (!rest)
 		return (NULL);
 	i = 0;
 	while (rest[i] && rest[i] != '\n')
+		i++;
+	new_line = malloc(sizeof(char) * (i + (rest[i] == '\n') + 1));
+	if (!new_line)
+		return (NULL);
+	i = 0;
+	while (rest[i] && rest[i] != '\n')
+	{	
+		new_line[i] = rest[i];
+		i++;
+	}
+	if (rest[i] && rest[i] == '\n')
+		new_line[i] = '\n';
+	new_line[i++] = '\0';
+	return (new_line);
+}
+
+char	*update_rest(char *rest)
+{
+	int		i;
+	int		j;
+	char	*new_rest;
+
+	if (!rest)
+		return (NULL);
+	i = 0;
+	while (rest[i] && rest[i] != '\0')
 		i++;
 	if (!rest)
 	{
 		free(rest);
 		return (NULL);
 	}
-	new_rest = (char *)malloc(sizeof(char) * ft_strlen(rest) - i + 1);
-	if (!new_rest)
-		return (NULL);
 	i++;
+	new_rest = malloc(sizeof(char) * (ft_strlen(rest) - i + 1));
+	if (!new_rest || !new_rest[i + 1])
+	{
+		free(rest);
+		return (NULL);
+	}
 	j = 0;
-	while (rest[i])
+	while (rest[++i])
 		new_rest[j++] = rest[i++];
-	new_rest[j] = '\0';
 	free(rest);
 	return (new_rest);
-}
-
-int main(void)
-{
-	int fd = open("teste.txt", O_RDONLY);
-
-	printf("%s", get_next_line(fd));
-	return (0);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 10:21:54 by clados-s          #+#    #+#             */
-/*   Updated: 2025/08/08 14:46:04 by clados-s         ###   ########.fr       */
+/*   Updated: 2025/08/11 16:28:25 by clados-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,83 +22,56 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
+char	*ft_strchar(char *s, int c)
+{
+	int	i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	while ((unsigned char)s[i] && (unsigned char)s[i] != (unsigned char)c)
+		i++;
+	if ((unsigned char)s[i] && (unsigned char)s[i] != (unsigned char)c)
+		return (NULL);
+	return ((char *)&s[i]);
+}
+
 char	*ft_strjoin_free(char *rest, char *buffer)
 {
 	char	*new_str;
 	int		i;
 	int		j;
-	int		len;
+	int		len_new;
 	
-	len = ft_strlen(rest) + ft_strlen(buffer);
-	new_str = malloc(len + 1 * sizeof(char));
+	len_new = ft_strlen(rest) + ft_strlen(buffer);
+	new_str = malloc(sizeof(char) * (len_new + 1));
 	if (!new_str)
-		return (0);
+		return (NULL);
 	i = -1;
-	j = -1;
-	while (rest[i++])
+	while (rest[++i])
 		new_str[i] = rest[i];
-	while (buffer[j++])
+	j = -1;
+	while (buffer[++j])
 		new_str[i + j] = buffer[j];
 	new_str[i + j] = '\0';
 	free(rest);
 	return (new_str);
 }
-
-char	*ft_strchar(char *str, int c)
+char	*ft_strdup(char *rest)
 {
-	int	i;
-	
-	i = 0;
-	while (str[i] && (unsigned char)str[i] != (unsigned char)c)
-		i++;
-	if (str[i] && (unsigned char)str[i] != (unsigned char)c)
-		return (NULL);
-	return ((char *)&str[i]);
-}
-
-char	*ft_strdup(char *str)
-{
-	int		len;
-	char	*dup;
 	int		i;
-
-	len = ft_strlen(str + 1);
-	dup = (char *)malloc(len);
-	if (!dup)
+	char	*new_str;
+	
+	if (!rest)
+		return (NULL);
+	new_str = malloc(sizeof(char) * ft_strlen(rest) + 1);
+	if (!new_str)
 		return (NULL);
 	i = -1;
-	while (str[++i])
-		dup[i] = str[i];
-	dup[i] = '\0';
-	return (dup);
+	while (rest[++i])
+		new_str[i] = rest[i];
+	new_str[i] = '\0';
+	return (new_str);
 }
 
-char	*ft_substr(char *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	char	*sub;
-	size_t	cpy_len;
-	size_t	s_len;
-	
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-		return (malloc(1 * sizeof(char)));
-	if (s_len - start < len)
-		cpy_len = s_len - start;
-	else
-		cpy_len = len;
-	sub = (char *)malloc(cpy_len + 1);
-	if (!sub)
-		return (NULL);
-	i = 0;
-	while (i < cpy_len)
-	{
-		sub[i] = s[i + start];
-		i++;
-	}
-	sub[i] = '\0';
-	return (sub);
-}
 
